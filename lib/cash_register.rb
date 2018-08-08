ada78e53c7864d7cb3b996d3a570d4eb26571c39
Arielle N Ramirez <ariellenramirez@gmail.com>
2018-08-08 14:25:17 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,15 @@
+  class CashRegister
+  
+  attr_accessor :discount, :total
+  
 
+  def initialize(total = 0, discount=20)
+    @total= total
+    @discount = discount
+    
+  end
+  
+  def add_item(title, price, sum)
+    self.total +=price * sum 
+end
+end
